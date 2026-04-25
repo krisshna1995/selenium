65029301c1d5de11a65576f20baa72c19b14c72a
krisshna1995 <101797522+krisshna1995@users.noreply.github.com>
2026-04-25 22:18:54 +0530

    chore: add BrowserType.java

---
+++ src/main/java/com/company/automation/core/driver/BrowserType.java
@@ -0,0 +1,6 @@
+// Enum of supported browser types (CHROME, FIREFOX, EDGE, SAFARI) for type safety.
+package com.company.automation.core.driver;
+
+public enum BrowserType {
+    CHROME, FIREFOX, EDGE, SAFARI
+}
