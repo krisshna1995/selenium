dae4250a067f5b335a1a9fe6f1c92cbef30c5cd9
krisshna1995 <101797522+krisshna1995@users.noreply.github.com>
2026-04-25 22:21:09 +0530

    chore: add FrameworkException.java

---
+++ src/main/java/com/company/automation/core/exceptions/FrameworkException.java
@@ -0,0 +1,5 @@
+// Custom framework runtime exception. Improves error messages over built-in exceptions.
+package com.company.automation.core.exceptions;
+
+public class FrameworkException extends RuntimeException {
+}
