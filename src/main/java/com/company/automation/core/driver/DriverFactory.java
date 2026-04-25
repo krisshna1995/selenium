70424d8b2c5d91bf79a9e42a535080cc74567aae
krisshna1995 <101797522+krisshna1995@users.noreply.github.com>
2026-04-25 22:09:40 +0530

    chore: add DriverFactory.java

---
+++ src/main/java/com/company/automation/core/driver/DriverFactory.java
@@ -0,0 +1,5 @@
+// Creates WebDriver instances (Chrome/Firefox/Edge/Safari/Remote). Centralizes browser options, headless mode, capabilities.
+package com.company.automation.core.driver;
+
+public class DriverFactory {
+}
